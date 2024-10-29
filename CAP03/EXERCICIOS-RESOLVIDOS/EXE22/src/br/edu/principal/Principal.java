2a78a9e6b1614a1b2d2eec9d56ea9c441c7ae322
João Victor Mendes Assunção <m.victor@aluno.ifce.edu.br>
2024-10-29 16:54:31 -0300

    Atividade 22

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE22/src/br/edu/principal/Principal.java
@@ -0,0 +1,25 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double vs, qkw, vkw, vr, d, vd;
+		vs = 265;
+		qkw = 222.17;
+		vkw = vs / 5;
+		vr = vkw *qkw;
+		d = vr * 15 / 100;
+		vd = vr - d;
+		System.out.println(vkw);
+		System.out.println(vr);
+		System.out.println(vd);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
