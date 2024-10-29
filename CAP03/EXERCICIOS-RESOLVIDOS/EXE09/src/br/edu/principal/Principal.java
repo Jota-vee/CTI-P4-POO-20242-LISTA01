17a427bac4b5208d92b60cf44dd4249dde51d761
João Victor Mendes Assunção <m.victor@aluno.ifce.edu.br>
2024-10-29 14:50:30 -0300

    Atividade 9

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE09/src/br/edu/principal/Principal.java
@@ -0,0 +1,12 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double base = 17.1717171722;
+		double altura = 17.171722;
+		System.out.print("A aréa é " + (base*altura)/ 2);
+
+	}
+
+}
