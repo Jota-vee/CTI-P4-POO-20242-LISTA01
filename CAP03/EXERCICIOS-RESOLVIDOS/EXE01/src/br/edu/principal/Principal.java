71c141a6c5249516dfe1f7b485d8af3a1106ad89
João Victor Mendes Assunção <m.victor@aluno.ifce.edu.br>
2024-10-29 14:47:09 -0300

    Atividade 1

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE01/src/br/edu/principal/Principal.java
@@ -0,0 +1,15 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int n1 = 8;
+		int n2 = 8;
+		int n3 = 8;
+		int n4 = 8;
+		int soma = n1 + n2 + n3 + n4;
+		System.out.println(soma);
+
+	}
+
+}
