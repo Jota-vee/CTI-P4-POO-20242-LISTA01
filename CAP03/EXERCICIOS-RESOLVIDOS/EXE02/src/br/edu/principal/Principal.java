b016cdf08a2e1bf818274fac59d16a30268bce03
João Victor Mendes Assunção <m.victor@aluno.ifce.edu.br>
2024-10-29 14:47:30 -0300

    Atividade 2

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE02/src/br/edu/principal/Principal.java
@@ -0,0 +1,14 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		float nota1 = 7;
+		float nota2 = 9;
+		float nota3 = 4;
+		float media = (nota1 + nota2 + nota3)/3;
+		System.out.print("A média foi " + media);
+		
+	}
+
+}
