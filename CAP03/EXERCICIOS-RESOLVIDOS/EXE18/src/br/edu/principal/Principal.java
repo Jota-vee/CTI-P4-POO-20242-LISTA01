646f371d64d2ad68f8d310c67d4cf66a97434eaf
João Victor Mendes Assunção <m.victor@aluno.ifce.edu.br>
2024-10-29 16:53:11 -0300

    Atividade 18

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE18/src/br/edu/principal/Principal.java
@@ -0,0 +1,23 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double ps, rg1, rg2, tf;
+		ps = 17;
+		rg1 = 22;
+		rg2 = 22;
+		rg1 = rg1 / 1000;
+		rg2 = rg2 / 1000;
+		tf = ps - 5 * (rg1 + rg2);
+		System.out.println(tf);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
