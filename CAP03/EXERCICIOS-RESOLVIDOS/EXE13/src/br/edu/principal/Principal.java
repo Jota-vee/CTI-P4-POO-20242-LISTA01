ce321c78cd6815425186ea1d5ffc762a661416a9
João Victor Mendes Assunção <m.victor@aluno.ifce.edu.br>
2024-10-29 16:51:12 -0300

    Atividade 13

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE13/src/br/edu/principal/Principal.java
@@ -0,0 +1,23 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double pes, polegadas, jardas , milhas;
+		pes = 17;
+		
+		polegadas = pes *12;
+		jardas = pes/3;
+		milhas = jardas/1760;
+		
+		System.out.print("a)"+polegadas+"  b)"+jardas+"  c)"+ milhas);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
