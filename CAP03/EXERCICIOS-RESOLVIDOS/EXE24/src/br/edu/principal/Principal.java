240c3a088852705cb5edc762d51dd3ec05b8ebce
João Victor Mendes Assunção <m.victor@aluno.ifce.edu.br>
2024-10-29 16:55:31 -0300

    Atividade 24

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE24/src/br/edu/principal/Principal.java
@@ -0,0 +1,20 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double hora, h, m, c;
+		hora = 17.17;
+		h = (int)hora;
+		m = hora - h;
+		c = (h*60) + (m*100);
+		System.out.println(c);
+		
+		
+		
+		
+		
+
+	}
+
+}
