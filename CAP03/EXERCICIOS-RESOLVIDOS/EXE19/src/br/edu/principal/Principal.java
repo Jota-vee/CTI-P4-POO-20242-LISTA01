b44d2d09388629ccad08b25f1539263c040b7fa9
João Victor Mendes Assunção <m.victor@aluno.ifce.edu.br>
2024-10-29 16:53:31 -0300

    Atividade 19

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE19/src/br/edu/principal/Principal.java
@@ -0,0 +1,20 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double degrau, usuario, qdegrau;
+		degrau = 0.25;
+		usuario = 2000;
+		qdegrau = usuario/degrau;
+		System.out.println(qdegrau);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
