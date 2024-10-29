d0dff2bf60079621864a4a0b735755144b49212c
João Victor Mendes Assunção <m.victor@aluno.ifce.edu.br>
2024-10-29 16:54:14 -0300

    Atividade 21

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE21/src/br/edu/principal/Principal.java
@@ -0,0 +1,21 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double x, y, z;
+		z = 3;
+		x = 2;
+		y = Math.pow(z, 2) - Math.pow(x, 2);
+		y = Math.pow(y, 0.5);
+		System.out.println(y);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
