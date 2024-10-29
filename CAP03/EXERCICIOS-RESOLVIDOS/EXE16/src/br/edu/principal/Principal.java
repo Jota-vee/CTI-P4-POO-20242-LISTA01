ba96da7a37a07e942805b60ba3d2f9b825b08ad9
João Victor Mendes Assunção <m.victor@aluno.ifce.edu.br>
2024-10-29 16:52:33 -0300

    Atividade 16

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE16/src/br/edu/principal/Principal.java
@@ -0,0 +1,26 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double hrt = 12;
+		double sala = 243.4;
+		double vlr_hr_t = sala/2;
+		double vlr_bru = vlr_hr_t * hrt;
+		double imp = vlr_bru * 3/100;
+		double sala_liq = vlr_bru - imp;
+		System.out.println(sala_liq);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
