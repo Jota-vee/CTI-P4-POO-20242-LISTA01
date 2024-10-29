00ac3c323d082d6059abd667b31965f70014234c
João Victor Mendes Assunção <m.victor@aluno.ifce.edu.br>
2024-10-29 16:55:49 -0300

    Atividade 25

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE25/src/br/edu/principal/Principal.java
@@ -0,0 +1,19 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double custo, convite, qtd;
+		custo = 17;
+		convite = 22;
+		qtd = custo / convite;
+		System.out.println(qtd);
+		
+		
+		
+		
+		
+
+	}
+
+}
