45d76d85399c9ab8085b3892b131d462bec1bd9d
João Victor Mendes Assunção <m.victor@aluno.ifce.edu.br>
2024-10-29 14:48:06 -0300

    Atividade 4

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE04/src/br/edu/principal/Principal.java
@@ -0,0 +1,12 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double salario = 1416.90;
+		double novo_salario = salario*1.25;
+		System.out.print("O novo sálario é "+ novo_salario);
+
+	}
+
+}
