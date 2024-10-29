5fd5de95c6dc2f40acdf41bb2a84b533ba003a0a
João Victor Mendes Assunção <m.victor@aluno.ifce.edu.br>
2024-10-29 14:50:51 -0300

    Atividade 10

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE10/src/br/edu/principal/Principal.java
@@ -0,0 +1,12 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double pi = 3.1415;
+		double raio = 17;
+		System.out.print("A Aréa é " + (pi * raio*raio));
+
+	}
+
+}
