82180da52db9eddf5b819fb0f90c54cdee996fa1
João Victor Mendes Assunção <m.victor@aluno.ifce.edu.br>
2024-11-04 10:42:57 -0300

    atividade 12

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE12/src/br/edu/principal/Principal.java
@@ -0,0 +1,13 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double n1 = 2;
+		double n2 = 3;
+		System.out.println(Math.pow(n1, n2));
+		System.out.println(Math.pow(n2, n1));
+
+	}
+
+}
